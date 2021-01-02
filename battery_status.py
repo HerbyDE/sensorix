a01da781ff18e09b85a4cea5ddfd68328efc1135
herbertwoisetschlager <herbert.woisetschlaeger@tum.de>
2021-01-02 19:13:30 +0100

    Added battery status.

---
+++ battery_status.py
@@ -0,0 +1,32 @@
+import os
+import sys
+import signal
+import time
+import socket
+
+# Adafruit libraries to address the i2c sensors
+import Adafruit_ADS1x15
+import Adafruit_SSD1306
+
+
+class ReadPower(object):
+
+    def __init__(self):
+        self.i2c_bus = 0x56
+        self.min_volt = 3*3.3 # Min 9.9V
+        self.max_volt = 3*4.2 # Max 12.6V
+        self.volt_gain = 1
+        self.adc = Adafruit_ADS1x15.ADS1115()
+
+    def measure_voltage(self):
+        measure = self.adc.read_adc(channel=0, gain=self.volt_gain)
+
+        return measure
+
+    def print_measure(self):
+        print(self.measure_voltage())
+
+
+if __name__ == '__main__':
+    pr = ReadPower()
+    pr.print_measure()
