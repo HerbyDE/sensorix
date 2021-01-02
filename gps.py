3bc7bcbfbbe7051214f09443c04077fc5378f4ee
herbertwoisetschlager <herbert.woisetschlaeger@tum.de>
2021-01-02 19:07:57 +0100

    Initial commit.

---
+++ gps.py
@@ -0,0 +1,7 @@
+
+
+
+class WireGPSSensor(object):
+
+    def __init__(self):
+        self.port = "tty/" # TODO: Set RPi Serial Port
---
+++ oled_display/oled_display.py
@@ -0,0 +1,1 @@
+from oled_display import oled_i2c_driver as i2c
