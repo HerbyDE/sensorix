931475ddc9d0a95fe8019775eaf30a2a6ee92a69
herbertwoisetschlager <herbert.woisetschlaeger@tum.de>
2021-01-02 20:20:32 +0100

    Added NMEA sentence for voltage.

--- battery_status.py
+++ battery_status.py
@@ -12,18 +12,34 @@ class ReadPower(object):
 
     def __init__(self):
         self.i2c_bus = 0x56
-        self.min_volt = 3*3.3 # Min 9.9V
-        self.max_volt = 3*4.2 # Max 12.6V
+        self.min_volt = 11.9 # Volt. Empty
+        self.max_volt = 12.65 # Volt. Full
         self.volt_gain = 1
         self.adc = Adafruit_ADS1x15.ADS1115()
 
     def measure_voltage(self):
-        measure = self.adc.read_adc(channel=0, gain=self.volt_gain)
+        measure = self.adc.read_adc(channel=0, gain=self.volt_gain) # Value 17200 eq 12.20V
+        voltage_factor = 17200 / 12.2
+        actual_voltage = measure / voltage_factor
+        percentage = actual_voltage / self.max_volt
 
-        return measure
+        return percentage
+
+    def transform_to_nmea_sentence(self, key, value):
+        sentence = f"POV, {key}, {value}"
+
+        # Generate the checksum as required by the OpenVario Protocol.
+        packet = bytes(sentence)
+        checksum = 0
+        for bt in packet:
+            checksum ^= ord(bt)
+
+        return f"${sentence}*{hex(checksum)}"
 
     def print_measure(self):
-        print(self.measure_voltage())
+        voltage = self.measure_voltage()
+        msg = self.transform_to_nmea_sentence(key="V", value=voltage)
+        print(msg)
 
 
 if __name__ == '__main__':
