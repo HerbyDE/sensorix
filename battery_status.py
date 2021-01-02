181114d0d32890a5c46018375ad9c7f9738f68c4
herbertwoisetschlager <herbert.woisetschlaeger@tum.de>
2021-01-02 20:22:39 +0100

    Added NMEA sentence for voltage.

--- battery_status.py
+++ battery_status.py
@@ -29,7 +29,7 @@ class ReadPower(object):
         sentence = f"POV, {key}, {value}"
 
         # Generate the checksum as required by the OpenVario Protocol.
-        packet = bytes(sentence, encoding="utf-8")
+        packet = str(bytes(sentence, encoding="utf-8"))
         checksum = 0
         for bt in packet:
             checksum ^= ord(bt)
