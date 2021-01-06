35071f61bc37d0e8740da94a05bdc1046ce0dd22
herbertwoisetschlager <herbert.woisetschlaeger@tum.de>
2021-01-06 14:16:15 +0100

    Added BMP280 sensors to setup and changed sampling rate to 0.2 seconds.

--- barometric_pressure.py
+++ barometric_pressure.py
@@ -2,11 +2,14 @@ import busio
 import board
 import adafruit_bmp280
 
+from smbus2 import smbus2
+
 
 class Barometer(object):
 
     def __init__(self):
-        self.i2c_bus = 0x01
+        self.i2c_bus_addr = 0x01
+        self.i2c_bus = smbus2.SMBus(bus=self.i2c_bus_addr)
         self.i2c_sp_channel = 0x76
         self.i2c_dp_channel = 0x77
         self.i2c = busio.I2C(board.SCL, board.SDA)
@@ -18,7 +21,31 @@ class Barometer(object):
         # self.dp_i2c.sea_level_pressure = 1013.25
 
     def read_static_pressure(self):
-        return self.sp_i2c.pressure
+        return self.sp_i2c.sea_level_pressure
+
+    def read_adjusted_pressure(self):
+        '''
+        This returns the QNH for hight estimation.
+        :return:
+        '''
+
+        # Raw inputs from i2c bus
+        loc_pressure = self.sp_i2c.pressure
+        loc_temp = self.sp_i2c.pressure
+
+        # Adjustment coefficients
+        temp = [0, 0, 0]
+        pres = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+
+
+
+
+
+
+
+
+
 
     def read_temperature(self):
         return self.sp_i2c.temperature
