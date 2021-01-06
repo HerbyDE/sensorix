0e55cddcfa40ff85a646dc4a53ce77045b2439c4
herbertwoisetschlager <herbert.woisetschlaeger@tum.de>
2021-01-06 13:24:28 +0100

    Added BMP280 sensors to setup and changed sampling rate to 0.2 seconds.

--- barometric_pressure.py
+++ barometric_pressure.py
@@ -11,11 +11,11 @@ class Barometer(object):
         self.i2c_dp_channel = 0x77
         self.i2c = busio.I2C(board.SCL, board.SDA)
         self.sp_i2c = adafruit_bmp280.Adafruit_BMP280_I2C(i2c=self.i2c, address=self.i2c_sp_channel)
-        self.dp_i2c = adafruit_bmp280.Adafruit_BMP280_I2C(i2c=self.i2c, address=self.i2c_dp_channel)
+        # self.dp_i2c = adafruit_bmp280.Adafruit_BMP280_I2C(i2c=self.i2c, address=self.i2c_dp_channel)
 
         # Configure the default sea level pressure of 1013.25 hPa
         self.sp_i2c.sea_level_pressure = 1013.25
-        self.dp_i2c.sea_level_pressure = 1013.25
+        # self.dp_i2c.sea_level_pressure = 1013.25
 
     def read_static_pressure(self):
         return self.sp_i2c.pressure
@@ -23,5 +23,5 @@ class Barometer(object):
     def read_temperature(self):
         return self.sp_i2c.temperature
 
-    def read_dynamic_pressure(self):
-        return self.dp_i2c
+    # def read_dynamic_pressure(self):
+        # return self.dp_i2c
