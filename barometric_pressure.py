775cb17418c577a6e2f35d0cf851009e7a2f7b72
herbertwoisetschlager <herbert.woisetschlaeger@tum.de>
2021-01-06 16:40:05 +0100

    Added BMP280 sensors to setup and changed sampling rate to 0.2 seconds.

--- barometric_pressure.py
+++ barometric_pressure.py
@@ -21,14 +21,15 @@ class Barometer(object):
         # self.dp_i2c.sea_level_pressure = 1013.25
 
     def read_static_pressure(self):
-        return self.sp_i2c.sea_level_pressure
-
-    def read_adjusted_pressure(self):
         '''
         This returns the QNH for hight estimation.
         :return:
         '''
 
+        adj_pressure = self.sp_i2c.pressure * pow(1 - self.sp_i2c.altitude / 44330.0, 5.255)
+        return adj_pressure
+
+        '''
         # Raw inputs from i2c bus
         loc_pressure = self.sp_i2c.pressure
         loc_temp = self.sp_i2c.pressure
@@ -37,15 +38,26 @@ class Barometer(object):
         temp = [0, 0, 0]
         pres = [0, 0, 0, 0, 0, 0, 0, 0, 0]
 
+        # Get calibration data from BMP280 sensor
+        cal_data = smbus2.SMBus(self.i2c_bus_addr).read_i2c_block_data(self.i2c_sp_channel, 0x88, 24)
 
+        # Modify temperature coefficients as given in the BMP280 data sheet.
+        temp[0] = cal_data[1] * 256 + cal_data[0]
+        temp[1] = cal_data[3] * 256 + cal_data[2]
+        temp[2] = cal_data[5] * 256 + cal_data[4]
 
+        if temp[1] > 32767:
+            temp[1] -= 65536
+        if temp[2] > 32767:
+            temp[2] -= 65536
 
-
-
-
-
-
-
+        # Modify pressure coefficients as given in the BMP280 data sheet.
+        pres[0] = cal_data[7] * 256 + cal_data[6]
+        for idx in range(0, 8):
+            pres[idx+1] = cal_data[2*idx+9] * 256 + cal_data[2*idx+8]
+            if pres[idx+1] > 32767:
+                pres[idx+1] -= 65536
+        '''
 
     def read_temperature(self):
         return self.sp_i2c.temperature
