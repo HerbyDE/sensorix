0352cfef9fd7471c29df6983fc11f56030266fb5
herbertwoisetschlager <herbert.woisetschlaeger@tum.de>
2021-01-14 16:54:58 +0100

    Updated I2C mux settings.

--- hardware_tests/i2c_test.py
+++ hardware_tests/i2c_test.py
@@ -2,25 +2,35 @@
 This class contains all hardware-related tests for the I2C bus and the TCA9548a multiplexer.
 This code is distributed as is and no warranty is given regarding functionality, etc.
 """
+import board
 import smbus2
+import busio
+
+from adafruit_tca9548a import TCA9548A
 
 
 class I2CTests(object):
 
+    def __init__(self):
+        self.i2c = busio.I2C(scl=board.SCL, sda=board.SDA)
+        self.i2c_mux = TCA9548A(i2c=self.i2c, address=0x70)
+        self.sens_channels = [0x48, 0x70]
+
     def check_i2c_hardware(self):
         registered_devices = {}
-        for bus_no in range(20):
+
+        for mux_ch in self.i2c_mux.channels:
             try:
-                bus = smbus2.SMBus(bus_no)
-                print(f"Testing BUS {bus_no}")
+                ext_bus = self.i2c_mux[mux_ch]
+                bus = smbus2.SMBus(ext_bus)
                 for ch in range(128):
                     try:
                         bus.read_byte(ch)
-                        registered_devices[bus_no] = ch
+                        registered_devices[mux_ch] = ch
+                        print(f">> Sensor detected at MUX {mux_ch} - CH {ch}")
                     except:
                         pass
-                bus.close()
             except:
-                pass
+                print(f">> No sensor detected at MUX {mux_ch}.")
         print(f"{len(registered_devices.keys())} sensor(s) registered on I2C Bus.")
         return registered_devices
