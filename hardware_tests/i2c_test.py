01c61de9aa7d45ebe52a246ac58303df11773bd2
herbertwoisetschlager <herbert.woisetschlaeger@tum.de>
2021-01-14 11:56:46 +0100

    Added initial hardware tests.

--- hardware_tests/i2c_test.py
+++ hardware_tests/i2c_test.py
@@ -15,9 +15,9 @@ class I2CTests(object):
 
         for bus in range(1, 20):
             for channel in range(3, 77):
-                print(f"Checking {bus}: {channel}")
+                print(f"Checking {hex(bus)}: {hex(channel)}")
                 if channel is not 70:  # 0x70 is the MUX port.
-                    sensor = pi.i2c_open(hex(bus), hex(channel))
+                    sensor = pi.i2c_open(bus, channel)
                     try:
                         pi.i2c_read_byte(sensor)
                         print(f">> Device found at {hex(bus)}: {hex(channel)}!")
